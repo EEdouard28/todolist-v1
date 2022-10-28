c507a7beb7879149de4f8771e4cde67b6b7e5f16
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-10-28 16:01:00 -0400

    ejs package installed

--- app.js
+++ app.js
@@ -5,19 +5,29 @@ const bodyParser = require("body-parser");
 //Creating app constant
 const app = express();
 
+//Tells express to usse EJS as view engine...place below express
+app.set('view engine', 'ejs');
+
 //Home get Route
 app.get("/", function (req, res) {
+    
     var today = new Date();
     var currentDay = today.getDay();
+    var day = "";
 
     if (currentDay === 6 || currentDay === 0) {
-        res.sendFile(__dirname + "/weekend.html");
+        day = "Weekend";
+        //render file list & past variable kindofday to value of day
+        res.render("list", {kindOfDay: day});
+        
     } else {
-        res.sendFile(__dirname + "/weekday.html");
+        day = "Weekday";
     }
+    res.render("list", {kindOfDay: day});
 });
 
 //listening on port 3000
+//process.env.PORT || 
 app.listen(3000, function() {
     console.log("Server started on port 3000")
 });
